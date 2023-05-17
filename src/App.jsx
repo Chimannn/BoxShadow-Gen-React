c9652b112eb4fadb0ae354eec1a014ff1cce79eb
chiman <1164617068@qq.com>
2023-05-17 22:39:31 +0800

    feat: first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css"
+import Header from './components/Header'
+import Container from './components/Container1'
+
+function App() {
+	return (
+		<div className="App">
+			<Header />
+			<div className="mainApp">
+				<Container />
+			</div>
+		</div>
+	)
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div className="Header">
+            <div>shadowGen</div>
+        </div>
+    )
+}
+
+export default Header;
